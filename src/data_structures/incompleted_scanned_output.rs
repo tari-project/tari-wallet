dc626be2237d089e16b875f91ecd08dd0504f01c
Martins Erts <fp@tari.com>
2025-09-01 15:03:15 +0300

    Make incompleted_scanned_output.rs compile

--- src/data_structures/incompleted_scanned_output.rs
+++ src/data_structures/incompleted_scanned_output.rs
@@ -1,13 +1,15 @@
 use serde::{Deserialize, Serialize};
 use tari_common_types::types::{CompressedCommitment, CompressedPublicKey};
 use tari_crypto::compressed_key::CompressedKey;
-use tari_script::ExecutionStack;
-use tari_transaction_components::key_manager::TariKeyId;
-use tari_transaction_components::MicroMinotari;
-use tari_transaction_components::rpc::models::MinimalUtxoSyncInfo;
-use tari_transaction_components::transaction_components::{EncryptedData, MemoField};
+use tari_transaction_components::{
+    key_manager::TariKeyId,
+    rpc::models::MinimalUtxoSyncInfo,
+    transaction_components::{EncryptedData, MemoField},
+    MicroMinotari,
+};
 use tari_utilities::ByteArray;
-use crate::{HexError, WalletError};
+
+use crate::WalletError;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct IncompleteScannedOutput {
@@ -19,9 +21,13 @@ pub struct IncompleteScannedOutput {
     pub memo: MemoField,
 }
 
-impl IncompleteScannedOutput{
-    pub fn new(scanning_info: &ScanningOutputStruct, value: MicroMinotari, commitment_mask_key_id: TariKeyId, memo: MemoField) -> Result<Self, WalletError> {
-      
+impl IncompleteScannedOutput {
+    pub fn new(
+        scanning_info: &ScanningOutputStruct,
+        value: MicroMinotari,
+        commitment_mask_key_id: TariKeyId,
+        memo: MemoField,
+    ) -> Result<Self, WalletError> {
         Ok(Self {
             output_hash: scanning_info.min_info.output_hash.clone(),
             value,
@@ -34,7 +40,7 @@ impl IncompleteScannedOutput{
 }
 
 #[derive(Clone, Serialize, Deserialize)]
-pub struct ScanningOutputStruct{
+pub struct ScanningOutputStruct {
     pub min_info: MinimalUtxoSyncInfo,
     pub commitment: CompressedCommitment,
     pub encrypted_data: EncryptedData,
@@ -43,18 +49,19 @@ pub struct ScanningOutputStruct{
 
 impl TryFrom<MinimalUtxoSyncInfo> for ScanningOutputStruct {
     type Error = WalletError;
+
     fn try_from(mim_info: MinimalUtxoSyncInfo) -> Result<Self, Self::Error> {
         let commitment = CompressedCommitment::from_canonical_bytes(&mim_info.commitment)
             .map_err(|e| WalletError::DataError(e.to_string()))?;
-        let encrypted_data = EncryptedData::from_bytes(&mim_info.encrypted_data).map_err(|e| WalletError::DataError(e.to_string()))?;
+        let encrypted_data =
+            EncryptedData::from_bytes(&mim_info.encrypted_data).map_err(|e| WalletError::DataError(e.to_string()))?;
         let sender_offset_public_key = CompressedKey::from_canonical_bytes(&mim_info.sender_offset_public_key)
             .map_err(|e| WalletError::DataError(e.to_string()))?;
         Ok(Self {
             min_info: mim_info,
             commitment,
             encrypted_data,
-            sender_offset_public_key
+            sender_offset_public_key,
         })
-        
     }
 }
