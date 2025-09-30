bc5bba186c8d9fcfbfc5f3034033cf48795d98ad
SW van Heerden <swvheerden@gmail.com>
2025-09-30 14:54:11 +0200

    delete unused

--- src/lib.rs
+++ src/lib.rs
@@ -4,19 +4,16 @@
 //! including UTXO management, transaction validation, and key management.
 
 pub mod common;
-// pub mod crypto;
 pub mod data_structures;
 pub mod errors;
 pub mod events;
 pub mod extraction;
 pub mod hex_utils;
-// pub mod key_management;
 pub mod scanning;
 
 #[allow(dead_code)]
 pub mod storage;
 pub mod utils;
-// pub mod validation;
 pub mod wallet;
 #[cfg(target_arch = "wasm32")]
 pub mod wasm;
@@ -24,8 +21,6 @@ pub mod wasm;
 pub use errors::*;
 pub use extraction::*;
 pub use hex_utils::*;
-// pub use key_management::*;
 pub use scanning::*;
 pub use storage::*;
-// pub use validation::*;
 pub use wallet::*;
